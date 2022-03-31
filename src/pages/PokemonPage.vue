de6bfdd75a6468f4a28a0000ef444c17bdb52335
Erick Guzman <guzmi.dev@gmail.com>
2022-03-30 20:10:46 -0700

    :art: structure pokemon game

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>¿Quién es este pokémon?</h1>
+  <PokemonPicture :pokemonId="386" :showPokemon="true" />
+  <PokemonOptions :pokemons="pokmeonArr" />
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOptions from "@/components/PokemonOptions.vue";
+
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+  data() {
+    return {
+      pokmeonArr: [],
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokmeonArr = await getPokemonOptions();
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
